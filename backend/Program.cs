b570f92d0c7a60d941c785529b3ab4fed6281ba1
Jason Steinshouer <jason.steinshouer@gmail.com>
2024-06-28 17:08:06 +0000

    serve frontend app

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,7 +2,12 @@ using RecipeBox.Data;
 using RecipeBox.Models;
 using RecipeBox.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+	Args = args,
+	// Look for static files in webroot
+	WebRootPath = "Public"
+});
 
 // Add the RecipeContext
 builder.Services.AddSqlite<RecipeContext>("Data Source=RecipeBox.db");
@@ -14,7 +19,10 @@ app.CreateDbIfNotExists();
 
 var recipeService = app.Services.CreateScope().ServiceProvider.GetRequiredService<RecipeService>();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// app.MapGet("/", () => "Hello World!");
 app.MapGet("/api/recipes/{id}", (int id) => recipeService.GetById(id));
 app.MapGet("/api/recipes", () => recipeService.GetAll());
 app.MapPost("/api/recipes", (Recipe recipe) => recipeService.Create(recipe));
