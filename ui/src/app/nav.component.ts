2da6a8d577abc191eb4de815a5ec167261653d17
Jason Steinshouer <jason.steinshouer@gmail.com>
2024-06-28 17:07:37 +0000

    update nav to use router links

--- ui/src/app/nav.component.ts
+++ ui/src/app/nav.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   template: `
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">RecipeBox</a>
+    <a class="navbar-brand" routerLink="/" >RecipeBox</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarColor02">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="/">Recipes</a>
+          <a class="nav-link active" routerLink="/" routerLinkActive="active">Recipes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="/recipes/new">New</a>
+          <a class="nav-link active" routerLink="/recipes/new" routerLinkActive="active">New</a>
         </li>
       </ul>
       <form class="d-flex">
