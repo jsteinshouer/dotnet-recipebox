af6b81323b2edabfe3607c96c14d056d02ed448e
Jason Steinshouer <jason.stienshouer@gmail.com>
2025-01-07 20:41:00 -0600

    add index

--- ui/src/recipes/recipes-list.component.ts
+++ ui/src/recipes/recipes-list.component.ts
@@ -10,9 +10,21 @@ import { RecipeService } from './recipe.service';
   imports: [RouterLink, RouterLinkActive, SlicePipe],
   template: `
     <h2>Recipes</h2>
-
+    <div class="row row-cols-2 g-4">
+        <div class="col">
+            <ul class="pagination">
+               <li class="page-item"><a class="page-link" href="#" (click)="showAll()" >All</a></li>
+              @for ( letter of index; track $index ) {
+              <li class="page-item"><a class="page-link" href="#" (click)="filterRecipes( letter )" >{{letter}}</a></li>
+              }
+            </ul>
+      </div>
+      <div class="col">
+        <span class="float-end">{{filterCount}} of {{recipes.length}}</span>
+      </div>
+    </div>
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-      @for ( recipe of recipes; track recipe.id ) {
+      @for ( recipe of filteredRecipes; track recipe.id ) {
       <div class="col">
         <div class="card h-100">
           <div class="card-body">
@@ -33,12 +45,26 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [];
+  filteredRecipes: Recipe[] = [];
+  index = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  total: number = 0;
+  filterCount: number = 0;
   recipeService: RecipeService = inject(RecipeService);
 
     constructor() {
       this.recipeService.getRecipes().then((recipeData: Recipe[]) => {
-        this.recipes = recipeData;
+        this.recipes = recipeData.sort((a, b) => a.name.toUpperCase() < b.name.toUpperCase() ? -1 : 1 );
+        this.showAll();
       });
+    }
+
+    filterRecipes( letter: string ) {
+      this.filteredRecipes = this.recipes.filter( (item) => Array.from( item.name )[0].toUpperCase() == letter );
+      this.filterCount = this.filteredRecipes.length;
+    }
+    showAll() {
+      this.filteredRecipes = this.recipes;
+      this.filterCount = this.recipes.length;
     }
 
 }
